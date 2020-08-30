1f10e52d4bb3ff0f51e4737dd93d9d88c86953c8
Wong Kean Wah <2560596+kilowatt-@users.noreply.github.com>
2020-08-30 12:32:48 -0700

    blah

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import Route from "react-router-dom/es/Route";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login.jsx";
+import { Switch } from "react-router-dom";
 
 function App() {
   return (
       <div>
-          <Route path="/" component={Home} exact />
-          <Route path="/login" component={Login} />
+          <Switch>
+              <Route path="/" component={Home} exact />
+              <Route path="/login" component={Login} />
+              <Route component={() => (<div><h1>404 Not Found</h1></div>)} />
+          </Switch>
       </div>
 
   );
--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import {API_CONFIG} from "../../config/api";
+import UserContext from "../../context/UserContext";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import * as qs from "querystring";
+import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -31,9 +33,71 @@ const useStyles = makeStyles((theme) => ({
     submit: {
         margin: theme.spacing(3, 0, 2),
     },
+    error: {
+        color: 'red'
+    }
 }));
 
 const Login = () => {
+    const user = useContext(UserContext);
+
+    const [loggingIn, setLoggingIn] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [emailError, setEmailError] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+        if (event.target.value === '') {
+          setEmailError(true);
+        } else {
+            setEmailError(false);
+        }
+    }
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+
+        if (event.target.value === '') {
+            setPasswordError(true);
+        } else {
+            setPasswordError(false);
+        }
+    }
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (!loggingIn) {
+            setErrorMessage("");
+            setLoggingIn(true);
+
+            const config = {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    "Access-Control-Allow-Origin": "*"
+                }
+            }
+
+            const body = {
+                email,
+                password
+            }
+
+            try {
+                const response = axios.post(`${API_CONFIG.base_url}/users/login`, qs.stringify(body), config);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+    }
+
     const classes = useStyles();
 
     return (
@@ -46,7 +110,7 @@ const Login = () => {
                 <Typography component="h1" variant="h5">
                     Sign in
                 </Typography>
-                <form className={classes.form} noValidate ction={"POST"}>
+                <form className={classes.form} noValidate onSubmit={handleSubmit}>
                     <TextField
                         variant="outlined"
                         margin="normal"
@@ -56,6 +120,10 @@ const Login = () => {
                         label="Email Address"
                         name="email"
                         autoComplete="email"
+                        disabled={loggingIn}
+                        value={email}
+                        error={emailError}
+                        onChange={handleEmailChange}
                         autoFocus
                     />
                     <TextField
@@ -67,11 +135,11 @@ const Login = () => {
                         label="Password"
                         type="password"
                         id="password"
+                        error={passwordError}
+                        disabled={loggingIn}
                         autoComplete="current-password"
-                    />
-                    <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="Remember me"
+                        value={password}
+                        onChange={handlePasswordChange}
                     />
                     <Button
                         type="submit"
@@ -79,21 +147,18 @@ const Login = () => {
                         variant="contained"
                         color="primary"
                         className={classes.submit}
+                        disabled={loggingIn}
                     >
-                        Sign In
+                        {loggingIn ? <CircularProgress color={"secondary"}/> : "Sign In"}
                     </Button>
                     <Grid container>
-                        <Grid item xs>
-                            <Link href="#" variant="body2">
-                                Forgot password?
-                            </Link>
-                        </Grid>
                         <Grid item>
-                            <Link href="#" variant="body2">
+                            <Link href="/signup" variant="body2">
                                 {"Don't have an account? Sign Up"}
                             </Link>
                         </Grid>
                     </Grid>
+                    <p className={classes.error}>{errorMessage}</p>
                 </form>
             </div>
         </Container>);
