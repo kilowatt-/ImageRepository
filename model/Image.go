40bc46c4e5a178ac37a43a8f68b79841506fac61
Wong Kean Wah <2560596+kilowatt-@users.noreply.github.com>
2020-08-30 22:31:36 -0700

    added Image struct

---
+++ model/Image.go
@@ -0,0 +1,14 @@
+package model
+
+// TODO: Add enums here
+type Image struct {
+	ID string
+	AuthorID string
+	Width int
+	Height int
+	FileType string
+	URL string
+	AccessLevel string
+	AccessListType string
+	AccessListIDs []string
+}
