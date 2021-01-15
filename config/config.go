69e28fda6b096ed758b098648711af0769ed99d8
Wong Kean Wah <2560596+kilowatt-@users.noreply.github.com>
2021-01-15 11:45:14 -0800

    added check for local environment flag

--- config/config.go
+++ config/config.go
@@ -2,11 +2,13 @@ package config
 
 import (
 	"github.com/joho/godotenv"
+	"log"
 	"os"
 )
 
 func InitializeEnvironmentVariables() error {
 	if _, productionExists := os.LookupEnv("PRODUCTION"); !productionExists {
+		log.Println("Not in production environment; loading local .env file")
 		if err := godotenv.Load(); err != nil {
 			return err
 		}
