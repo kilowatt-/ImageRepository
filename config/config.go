2dc2dee9d82b191a19ef15220412e925a3f849ee
Wong Kean Wah <2560596+kilowatt-@users.noreply.github.com>
2021-01-15 11:40:04 -0800

    added check for production flag

--- config/config.go
+++ config/config.go
@@ -2,11 +2,14 @@ package config
 
 import (
 	"github.com/joho/godotenv"
+	"os"
 )
 
 func InitializeEnvironmentVariables() error {
-	if err := godotenv.Load(); err != nil {
-		return err
+	if _, productionExists := os.LookupEnv("PRODUCTION"); !productionExists {
+		if err := godotenv.Load(); err != nil {
+			return err
+		}
 	}
 
 	return nil
