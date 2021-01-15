0a45e88b1cfa2b22fef8ffb4b3f61f59bd1f5e4a
Wong Kean Wah <2560596+kilowatt-@users.noreply.github.com>
2021-01-15 11:09:28 -0800

    removed frontend

--- client/src/config/api.js
+++
@@ -1,3 +0,0 @@
-export const API_CONFIG = {
-    base_url: (process.env.NODE_ENV === 'production') ? '/api' : 'http://localhost:25000/api'
-}
--- routes/routes.go
+++ routes/routes.go
@@ -2,36 +2,10 @@ package routes
 
 import (
 	"github.com/gorilla/mux"
-	"github.com/kilowatt-/ImageRepository/routes/common"
 	"github.com/kilowatt-/ImageRepository/routes/images"
 	"github.com/kilowatt-/ImageRepository/routes/users"
 	"net/http"
-	"os"
-	"path/filepath"
 )
-
-func serveSPA(w http.ResponseWriter, r *http.Request) {
-	staticPath := "./client/build"
-	indexPath := "index.html"
-
-	path, err := filepath.Abs(r.URL.Path)
-
-	if err != nil {
-		http.Error(w, "Bad path sent", http.StatusBadRequest)
-	} else {
-		path = filepath.Join(staticPath, path)
-
-		_, err = os.Stat(path)
-
-		if os.IsNotExist(err) {
-			http.ServeFile(w, r, filepath.Join(staticPath, indexPath))
-		} else if err != nil {
-			common.SendInternalServerError(w)
-		} else {
-			http.FileServer(http.Dir(staticPath)).ServeHTTP(w, r)
-		}
-	}
-}
 
 func serveCatchAllAPIRoutes(r *mux.Router) {
 	r.PathPrefix("/api").HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
@@ -39,14 +13,10 @@ func serveCatchAllAPIRoutes(r *mux.Router) {
 	})
 }
 
-func serveFrontEnd(r *mux.Router) {
-	r.PathPrefix("/").HandlerFunc(serveSPA)
-}
 
 func RegisterRoutes(r *mux.Router) {
 	users.ServeUserRoutes(r.PathPrefix("/api/users").Subrouter())
 	images.ServeImageRoutes(r.PathPrefix("/api/images").Subrouter())
 	serveCatchAllAPIRoutes(r)
-	serveFrontEnd(r)
 }
 
